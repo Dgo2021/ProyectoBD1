4cbc0f669c37b37ec089b7c1e7ab53250c2ed0d1
Diego Esaú Chinchilla Solares <chinchillaesau@gmail.com>
2021-10-13 20:34:45 -0600

    OneToMany--Clinica--Depto

--- BackEnd/proyecto/src/main/java/umg/bd/proyecto/model/Clinica.java
+++ BackEnd/proyecto/src/main/java/umg/bd/proyecto/model/Clinica.java
@@ -1,30 +1,58 @@
 package umg.bd.proyecto.model;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class Clinica {
+@Table(name= "Clinica")
+public class Clinica implements Serializable {
+	
+	private static final long serialVersionUID = 2L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Basic(optional = false)
+	
+	@Column(name= "idclinica")
 	private Integer IdClinica;
-	private Integer IdDepartamento;
+	
+	@OneToMany(mappedBy = "iddepartamento")
+	private List<Departamento> iddepartamentoList;
+	
+	//private Integer IdDepartamento;
+	@Column(name= "clinica")
 	private String Clinica;
+	
+	@Column(name= "direccion")
 	private String Direccion;
+	
+	public List<Departamento> getIddepartamentoList() {
+		return iddepartamentoList;
+	}
+	public void setIddepartamentoList(List<Departamento> iddepartamentoList) {
+		this.iddepartamentoList = iddepartamentoList;
+	}
 	public Integer getIdClinica() {
 		return IdClinica;
 	}
 	public void setIdClinica(Integer idClinica) {
 		IdClinica = idClinica;
 	}
-	public Integer getIdDepartamento() {
+	/*public Integer getIdDepartamento() {
 		return IdDepartamento;
 	}
 	public void setIdDepartamento(Integer idDepartamento) {
 		IdDepartamento = idDepartamento;
-	}
+	}*/
 	public String getClinica() {
 		return Clinica;
 	}
--- BackEnd/proyecto/src/main/java/umg/bd/proyecto/model/Departamento.java
+++ BackEnd/proyecto/src/main/java/umg/bd/proyecto/model/Departamento.java
@@ -1,27 +1,49 @@
 package umg.bd.proyecto.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class Departamento {
+@Table(name= "Departamento")
+public class Departamento implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer IdDepartamento;
+	@Basic(optional = false)
+	
+	//private Integer IdDepartamento;
+	@Column(name="departamento")
 	private String Departamento;
-	public Integer getIdDepartamento() {
+	
+	@Column(name = "iddepartamento")
+	private Integer iddepartamento;
+	
+	/*public Integer getIdDepartamento() {
 		return IdDepartamento;
 	}
 	public void setIdDepartamento(Integer idDepartamento) {
 		IdDepartamento = idDepartamento;
-	}
+	}*/
 	public String getDepartamento() {
 		return Departamento;
 	}
 	public void setDepartamento(String departamento) {
 		Departamento = departamento;
+	}
+	public Integer getIddepartamento() {
+		return iddepartamento;
+	}
+	public void setIddepartamento(Integer iddepartamento) {
+		this.iddepartamento = iddepartamento;
 	}
 	
 }
