0f335d7cb2cfac77e358513eee1daa9f85c29683
Diego Esaú Chinchilla Solares <chinchillaesau@gmail.com>
2021-10-13 23:27:03 -0600

    Create ClinicaController.java

---
+++ BackEnd/proyecto/src/main/java/umg/bd/proyecto/controller/ClinicaController.java
@@ -0,0 +1,25 @@
+package umg.bd.proyecto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import umg.bd.proyecto.model.Clinica;
+import umg.bd.proyecto.repository.ClinicaRepository;
+
+@RestController
+@RequestMapping("/clinica")
+@CrossOrigin
+public class ClinicaController {
+	@Autowired
+	private ClinicaRepository clinicaRepository;
+	
+	@GetMapping(path="/all")
+	  public @ResponseBody Iterable<Clinica> getAll() {
+	    return clinicaRepository.findAll();
+	  }
+}
+	
