dea5f2c8141c3e5746d5d6c3e7ef3f19673cc47c
Diego Esaú Chinchilla Solares <chinchillaesau@gmail.com>
2021-10-24 15:44:21 -0600

    Update Physical y Conceptual V1.3 | Factura

--- FrontEnd/src/app/factura/factura.component.ts
+++ FrontEnd/src/app/factura/factura.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../models/Empleado.interface';
+import { EmpleadoService } from '../service/empleado.service';
 
 @Component({
   selector: 'app-factura',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacturaComponent implements OnInit {
 
-  constructor() { }
+  empleados: any = [];
+  emp:Empleado[]= [];
+  selectedValue:any=1;
+  clientes: any = [];
+  personas: any = [];
+  clinicas: any = [];
+  
+
+  constructor(private empleadoService: EmpleadoService) {
+    
+   }
 
   ngOnInit(): void {
   }
+
+  mostrarEmpleados(req: any) {
+    this.empleados = req;
+    console.log(this.empleados);
+  }
+
+  getEmp(data:Empleado[]){
+    this.emp=data;
+  }
+
+  selectValue(ev:any){
+    this.selectedValue=ev;
+ }
+
+ listaPersonas(req: any) {
+  this.personas = req;
+  console.log(this.personas);
+}
+
 
 }
